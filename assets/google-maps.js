8211d347fbc605834af26d678dc4f8cf0eafdfc3
Mishsmelle <michelle.oconnor@shopify.com>
2021-06-01 18:20:02 +0000

    added Google Map and changed all Medoc references to Champagne

--- assets/google-maps.js
+++ assets/google-maps.js
@@ -1,149 +1,69 @@
-// dynamically creates and loads buttons in the map section using data from database.js
-window.addEventListener('load', function () {
-    const buttonContainer = document.getElementById('button-row');
-    for (let buttonName of Object.keys(jewelleryQuarterPlaces)) {
-        buttonContainer.innerHTML += 
-        `<button type="button" class="button-bg" aria-label="${buttonName}" 
-        onclick="dropMarkers(jewelleryQuarterPlaces.${buttonName}); setDefaultText();">
-            <i class="fas ${placeIcon[buttonName]}"></i>
-            <span class="d-none d-md-inline"> ${buttonName}</span>
-        </button>`;
-    }
-    setDefaultText();
-});
-
-const reccomendContentSection = document.getElementById('recommend-content');
-/** sets the default html in recommend-content class on load */
-function setDefaultText() {
-    reccomendContentSection.innerHTML =
-    `<h3 class="recommend-content-title">Visit JQ Recommends</h3>
-    <hr class="divider">
-    <p class="recommend-content-p">
-        Museums, heritage, creativity and culture. The JQ is bursting with 
-        activities and new experiences.
-    </p>
-    <hr>
-    <p class="recommend-content-p">
-        Use our interactive map to help plan your visit and view our top picks. Click 
-        on one of the buttons above the map to start!
-    </p>`;
-}
-
-// walkthroughs & tutorials: 
-// Eamonn Smyth - How to Google maps PDF via slack & Pradip Debnath - https://youtu.be/Xptz0GQ2DO4
-// Google Maps JavaScript API documentation | Developers console 
-// Sam Codes - https://youtu.be/uPhWSyRqQDA & Traversy Media - https://youtu.be/Zxf1mnP5zcw
-
-let map;
-// we use markersArray to show & delete multiple markers
-let markersArray = [];
-let infoObj = [];
-
-// jQ is the co-ordinates of the center point
-const jQ = {
-    lat: 52.487137777478495,
-    lng: -1.9097842445407542
-};
-
-/** initMap function is fired when the google map script is read in index.html*/
 function initMap() {
-    // renders map in #map div in html
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: jQ,
-        zoom: 14.5,
-        mapTypeId: 'terrain',
-        disableDefaultUI: true
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 3,
+      center: { lat: -28.024, lng: 140.887 },
     });
-}
-
-/**
- * places markers on the map
- * called via eventListeners which pass in placeType
- * @param {Array} placeType - extracted from jewelleryQuarterPlaces object in database.js
- */
-function dropMarkers(placeType) {
-    map.panTo(jQ);
-    map.setZoom(14.5);
-    clearMarkers();
-    for (let i = 0; i < placeType.length; i++) {
-        addMarker(placeType[i]);
-    }
-}
-
-/**
- * collects and uses data from the object to position markers 
- * adds and shows info via the infoWindows via marker eventListener
- * changes the content in the recommend-content div to a review or additonal info
- * @param {Object} place - extracted from placeType array, which holds all the data of each place
- */
-function addMarker(place) {
-    const marker = new google.maps.Marker({
-        position: place.location,
-        map: map,
-        title: place.name,
-        icon: place.iconImage
+    // Create an array of alphabetical characters used to label the markers.
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // Add some markers to the map.
+    // Note: The code uses the JavaScript Array.prototype.map() method to
+    // create an array of markers based on a given "locations" array.
+    // The map() method here has nothing to do with the Google Maps API.
+    const markers = locations.map((location, i) => {
+      return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length],
+      });
     });
-    // push marker info to markersArray which we use to show & delete multiple markers
-    markersArray.push(marker);
-
-    const infowindow = new google.maps.InfoWindow({
-        maxWidth: 400,
-        content: `<div class="info-window">
-            <h5>${place.name}</h5>
-            <h6>${place.title}</h6>
-            <p>${place.desc}</p>
-            <a href="${place.website}" target="_blank" rel="noreferrer">
-                Click here to visit website
-            </a>
-        </div>`
+    // Add a marker clusterer to manage the markers.
+    new MarkerClusterer(map, markers, {
+      imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
+  }
+  const locations = [
+    //Bordeaux
+    { lat: 44.839168372440774, lng: -0.5763232548853524  },
+    //hotels
+    { lat:44.83858018806471, lng:-0.5797125306494817 },
+    { lat: 44.844544375552296,  lng: -0.5592848268138768 },
+    { lat: 44.84664072704814, lng: -0.5841927859625233 },
+//restaurants
+    { lat: 44.84160106024529, lng:-0.5905362102719739  },
+    { lat: 44.83268452465521, lng: -0.5960722896728019 },
+    { lat: 44.85121598219748, lng:-0.5621262524165621 },
+//bars
+    { lat:44.84015795462957, lng: -0.5693088475981463 },
+    { lat: 44.84381629264415, lng: -0.5707657657040709 },
+    { lat: 44.84168811583788, lng: -0.5818405341746097 },
 
-    marker.addListener('click', function () {
-        map.panTo(place.location);
-        closeWindows();
-        infowindow.open(map, marker);
-        // sets itself to infoObj[0] that we clear using closeWindows()
-        infoObj[0] = infowindow;
-        changeDefaultText(place);
-    });
-}
+    //Champagne
+    { lat: 45.837058438142286, lng: -0.9175872524314757 },
+    //hotels
+    { lat: 45.81055048376134, lng: -0.9574634341748279 },
+    { lat: 45.8229823746349, lng: -0.7842571216910094 },
+    { lat: 45.894264124086526, lng: -0.9171545608193644 },
+//restaurants
+    { lat: 45.893173559289274, lng: -0.9159314928926761  },
+    { lat: 445.906254644006744, lng: -0.9617650804902288 },
+    { lat: 45.92566153241454, lng: -0.9585893450199864 },
+//bars
+    { lat: 45.91455297834956, lng:  -0.8887231688426446 },
+    { lat: 45.875545373339015, lng: -0.9633100370491485 },
+    { lat: 45.875844157413205, lng:  -0.9634816984259185 },
 
-/** this function loops through markersArray and deletes existing markers by clearing the array */
-function clearMarkers() {
-    for (let i = 0; i < markersArray.length; i++) {
-        markersArray[i].setMap(null);
-    }
-    markersArray = [];
-}
-
-/** this function closes previously opened infoWindows by clearing the infoObj array */
-function closeWindows() {
-    if (infoObj.length > 0) {
-        infoObj[0].close();
-        infoObj[0].length = 0;
-    }
-}
-
-/**
- * replaces the default html in recommend-content div with either a reveiw or additional info
- * @param {Object} place - extracted from placeType array, which holds the data of each place
- */
-function changeDefaultText(place) {
-    if (place.reviewTitle && place.review) {
-        reccomendContentSection.innerHTML =
-        `<h3 class="recommend-content-title">${place.reviewTitle}</h3>
-        <hr class="divider">
-        <p class="recommend-content-p">${place.review}</p>`;
-    } else if (place.addInfo) {
-        reccomendContentSection.innerHTML =
-        `<h3 class="recommend-content-title">${place.name}</h3>
-        <hr class="divider">
-        <p class="recommend-content-p">${place.addInfo}</p>
-        <hr>
-        <a href="${place.website}" target="_blank rel="noreferrer"">
-            Click here for timetables, journey planners and any other additional info.
-        </a>`;
-    } else {
-        setDefaultText();
-    }
-}
+    //Burgundy
+    { lat:47.05400393088885, lng: 4.383269628922687 },
+    //hotels
+    { lat: 47.20587657765813, lng: 4.62156830021535 },
+    { lat: 47.09099746469766, lng: 4.24322662581458 },
+    { lat: 46.94962744536695, lng: 4.7334915178692984 },
+//restaurants
+    { lat: 47.03193983763646,  lng: 4.830028016452519 },
+    { lat: 447.19315944926542, lng: 4.9508776256985 },
+    { lat: 47.087604098946564, lng: 4.2422594623925205 },
+//bars
+    { lat: 46.91991803654484, lng:  4.749970749930295 },
+    { lat: 47.032826031331396, lng: 4.835801438315226 },
+    { lat: 47.03445061959454, lng:  4.836208086082757 },
+  ];
